d3ae37ee9979928d191fbb6d086633de739063cf
BrianMorga <bmorga@ggc.edu>
2023-04-12 20:35:35 -0400

    added constructor and getter for passiveKey class

--- src/PassiveKey.java
+++ src/PassiveKey.java
@@ -1,4 +1,12 @@
 import java.io.Serializable;
 
 public class PassiveKey extends Key implements Serializable {
+        
+        public PassiveKey(int ItemID, String ItemName, String ItemDesc, int ItemLocation) {
+            super(ItemID, ItemName, ItemDesc, ItemLocation);
+        }
+
+        public boolean doesPlayerHaveItem() {
+            return false;
+        }
 }

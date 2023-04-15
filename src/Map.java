c1f312b40d956945e7c97176ca10e5dc4bdb25b1
hmbarnez <hmbarnes007@gmail.com>
2023-04-15 11:18:47 -0400

    added game information text files -harrison

--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,11 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+
 
 public class Map implements Serializable {
+    private ArrayList<Room> roomArrayList;
+    private ArrayList<Item> itemsArrayList;
+    private ArrayList<Monster> monstersArrayList;
+    private ArrayList<Puzzle> puzzlesArrayList;
 }
+

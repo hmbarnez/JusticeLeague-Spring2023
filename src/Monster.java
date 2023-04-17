0a8697ec8d0aba55a40ed20618826660fe37b7d7
Adrian <ajapa530@gmail.com>
2023-04-17 13:39:37 -0400

    Made skeleton Monster class

--- src/Monster.java
+++ src/Monster.java
@@ -1,4 +1,31 @@
 import java.io.Serializable;
+/*
+ * Author: Adrian Japa
+ * This class is in charge of creating the monsters that will be used in the game.
+ */
+private String name;
+private String dialogue;
+private boolean isAlive;
+private int attackDmg;
+private int health;
 
-public class Monster implements Serializable {
+public class Monster implements Serializable 
+{   
+    // constructor
+    public Monster(String name, String dialogue, int attackDmg, int health)
+    {
+        this.name = name;
+        this.dialogue = dialogue;
+        this.attackDmg = attackDmg;
+        this.health = health;
+        this.isAlive = true;
+    }
+
+    // getters and setters
+
+    //Method of attack to target
+    public void attack()
+    {
+        //TODO
+    }
 }

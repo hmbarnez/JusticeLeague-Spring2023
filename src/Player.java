06865545bdd1c02b7a59ce50c6e58d84d69a7a7c
Niecia <nsay@ggc.edu>
2023-04-21 00:02:53 -0400

    added methods:methods in Player class

--- src/Player.java
+++ src/Player.java
@@ -19,4 +19,82 @@ public class Player implements Serializable {
         this.maxHP = 100;
         this.attackDmg = 10;
     }
+
+    //move rooms: associated with numbers
+/*
+    public void move(String direction){
+        Room currentRoom = this.rooms.get(this.getCurrentRoomID() - 1);
+        Room nextRoom = null;
+
+        if ("n".equals(direction)) {
+            nextRoom
+        } else if ("s".equals(direction)) {
+            nextRoom
+        } else if ("e".equals(direction)) {
+            nextRoom
+        } else if ("w".equals(direction)) {
+            nextRoom
+        } else {
+            System.out.println("Wrong input");
+            return;
+        }
+
+        if(nextRoom == null){
+            System.out.println("No Room");
+            return;
+        }
+
+        //updates room id
+        this.setCurrentRoomID(nextRoom.getRoomId());
+        //prints the description of the room being moved to
+        System.out.println(nextRoom.getDescription() + " " + nextRoom.getRoomId());
+
+        //check to see if the room has been visited
+        if (nextRoom.isVisited()){
+            System.out.println("This room is familiar...");
+        }
+
+        //update the isVisited
+        nextRoom.setVisited();
+    }
+    */
+    //search room : inspect room:see and pick up item
+    public void exploreRoom(){
+        Room currentRoom = this.rooms.get(this.getCurrentRoomID()-1);
+        if (currentRoom.getRoomInventory().isEmpty())
+        {
+            System.out.println("There is no item in this room.");
+        } else {
+            System.out.println("List of items in this room " + currentRoom.getRoomInventory());
+        }
+
+    }
+
+    public int getCurrentRoomID() {
+        return currentRoomID;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public ArrayList<Item> getPlayerInventory() {
+        return playerInventory;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public int getAttackDmg() {
+        return attackDmg;
+    }
+
+    public Armor getArmor() {
+        return armor;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
 }
